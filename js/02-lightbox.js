7b8e4a6ed02d8cacff6e5748011bb83a46f7be31
natalia-ya <sagaidak.nata@gmail.com>
2021-11-19 00:09:11 +0200

    Update 02-lightbox.js

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,25 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryContainer = document.querySelector('.gallery');
+
+const createitem = galleryItems
+  .map(
+    ({ preview, original, description }) =>
+      `<li>
+        <a class="gallery__item" href="${original}"
+            style='display: block; height: 240px'>
+          <img class="gallery__image" src="${preview}" alt="${description}" />
+        </a>
+    </li>`
+  )
+  .join('');
+
+galleryContainer.insertAdjacentHTML('beforeend', createitem);
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: `250`,
+  captionPosition: 'bottom',
+});
