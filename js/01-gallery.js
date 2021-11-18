29e2e005d68cf38ef8ee92e2c5300d18c83a1437
natalia-ya <sagaidak.nata@gmail.com>
2021-11-19 00:09:07 +0200

    Update 01-gallery.js

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,3 +2,64 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryContainer = document.querySelector('.gallery');
+
+const createitem = galleryItems
+  .map(
+    ({ preview, original, description }) =>
+      `<div class="gallery__item">
+        <a class="gallery__link" href="${original}">
+            <img
+                class="gallery__image"
+                src="${preview}"
+                data-source="${original}"
+                alt="${description}"
+            />
+        </a>
+    </div>`
+  )
+  .join('');
+
+galleryContainer.insertAdjacentHTML('beforeend', createitem);
+
+galleryContainer.addEventListener('click', openImage);
+
+function openImage(event) {
+  event.preventDefault();
+
+  // Фильтр цели
+  const targetImg = event.target.nodeName === 'IMG';
+  if (!targetImg) {
+    return;
+  }
+
+  const instance = basicLightbox.create(
+    `<div class="modal">
+      <img src = ${event.target.dataset.source}
+        alt=${event.target.alt}
+        width = '1140'
+        height = '800'
+      />
+    </div> `,
+
+    {
+      onShow: instance => {
+        instance.element().querySelector('img').onclick = instance.close;
+
+        window.addEventListener('keydown', eventHandler);
+      },
+      onClose: instance => {
+        window.removeEventListener('keydown', eventHandler);
+      },
+    }
+  );
+  function eventHandler(event) {
+    if (event.key === 'Escape') {
+      instance.close();
+      return;
+    }
+  }
+
+  instance.show();
+}
